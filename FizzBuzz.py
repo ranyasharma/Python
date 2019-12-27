34669c2ef7d2c8492a0fcd16ab99fdee6855eb84
ranyasharma <52800876+ranyasharma@users.noreply.github.com>
2019-12-27 15:28:43 -0600

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+﻿#fizz buzz program (prints numbers 1- 100 and "fizz" for those with factor of 3, "buzz" for those with factor of 5, and "fizzbuzz" for those with factors of both)
+
+for i in range(1,100,1) :
+    if (i % 5 == 0) and(i % 3 == 0) :
+
+        print("Fizz Buzz")
+    else: 
+        
+        if i % 3 == 0 :
+            print("Fizz")
+        
+        else:
+            if i % 5 == 0 :
+                print("Buzz")
+            else: 
+                print(i)
---
+++ PrimeNumbers.py
@@ -0,0 +1,15 @@
+﻿#prints numbers 1- 100 and lists them as odd, even, or prime 
+for i in range (1,100,1) :
+    count = 0 
+    for num in range (2,i-1,1):
+        if (i % num == 0):
+            count = count + 1
+    if count == 0:
+        print (i, "prime")
+    else:
+        if i % 2 == 0:
+            print (i, "even")
+        else:
+            if i % 2 == 1:
+                print (i, "odd")
+            
